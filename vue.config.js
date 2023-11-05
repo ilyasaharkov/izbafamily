d81e58470d99a93abe807bc71a8e98317e994145
Илья Лесунов <coolbiird@gmail.com>
2023-11-05 12:49:53 +0600

    - base

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  base: './',
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
       ? '/' + process.env.CI_PROJECT_NAME + '/'
